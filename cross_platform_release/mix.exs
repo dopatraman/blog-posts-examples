99642267645e415b156e2c66a187c545544e0c81
Prakash Venkat <venkatraman.prakash@gmail.com>
2019-11-21 13:48:27 -0800

    elixir docker image
    build container script

--- cross_platform_release/mix.exs
+++ cross_platform_release/mix.exs
@@ -5,7 +5,7 @@ defmodule CrossPlatformRelease.MixProject do
     [
       app: :cross_platform_release,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
